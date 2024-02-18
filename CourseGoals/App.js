9cc0601f9b7e2d903acee51b135b85a2f4f1360b
Sarp Bozkurt <mubozkt@gmail.com>
2024-02-18 20:10:14 +0300

    course goals start

---
+++ CourseGoals/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  return (
+    <View styles={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your Course Goal" />
+        <Button title="add your text goal" />
+      </View>
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+});
